f3cdfbf3128d71fafcecd7b9578cd8ee93daa65d
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-04-10 00:04:24 +0200

    updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,20 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
-        return view('products.index', compact('products'));
-    }
-
-    public function create()
-    {
-        return view('products.create');
+        return Product::all();
     }
 
     public function store(Request $request)
@@ -27,18 +21,12 @@ class ProductController extends Controller
             'project_id' => 'nullable|exists:projects,id'
         ]);
 
-        Product::create($request->all());
-        return redirect()->route('products.index')->with('success', 'Product created successfully.');
+        return Product::create($request->all());
     }
 
     public function show(Product $product)
     {
-        return view('products.show', compact('product'));
-    }
-
-    public function edit(Product $product)
-    {
-        return view('products.edit', compact('product'));
+        return $product;
     }
 
     public function update(Request $request, Product $product)
@@ -51,12 +39,12 @@ class ProductController extends Controller
         ]);
 
         $product->update($request->all());
-        return redirect()->route('products.index')->with('success', 'Product updated successfully.');
+        return $product;
     }
 
     public function destroy(Product $product)
     {
         $product->delete();
-        return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
+        return response()->json(['message' => 'Product deleted successfully']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -48,13 +48,11 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/project/members', [ProjectController::class, 'projectMembers']);
 
 
-    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{product}', [ProductController::class, 'show']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 
 
     Route::get('/tasks', [TasksController::class, 'index']);
